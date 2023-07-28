3f68c2445000b8defbe29b0bd403835508265c4a
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-07-28 13:26:41 +0300

    Added an optional validation step for images, added a pass for finishing aspect ratio translations

--- utils/data_filtering.py
+++ utils/data_filtering.py
@@ -3,36 +3,44 @@ import time
 from pathlib import Path
 from typing import Optional
 
+import numpy as np
 import pandas as pd
 import requests
+from tqdm import tqdm
 
 from dataset import IMG_EXTENSIONS
-from tqdm import tqdm
+
+np.random.seed(42)
 
 
 def download_midjourney_data(csv_path: Path,
                              dir_out: Path,
                              apply_filtering: bool = True,
                              csv_out: Optional[Path] = None,
-                             desired_size: Optional[int] = None) -> None:
+                             desired_size: Optional[int] = None,
+                             validate_data: bool = True) -> None:
     """
-    Download the MidJourney v5.1 Cleaned Dataset with filters.
-    Download the CSV from https://www.kaggle.com/datasets/iraklip/modjourney-v51-cleaned-data.
+    Download the Midjourney v5.1 Cleaned Dataset with filters.
+    Download the CSV file from https://www.kaggle.com/datasets/iraklip/modjourney-v51-cleaned-data.
     Code adapted from https://www.kaggle.com/code/iraklip/downloading-midjourney-v5-1-images.
 
+    The CSV file contains metadata used to download the images from the internet.
+    A filtered CSV is optionally created from the original one before downloading.
+
     Args:
-        csv_path: Path to the MidJourney CSV file, or an already filtered one
+        csv_path: Path to the Midjourney CSV file, or an already filtered one
         dir_out: Output directory for the images
         apply_filtering: Whether to filter the data
         csv_out: Path for a CSV file to save the filtered data
         desired_size: Desired number of images
+        validate_data: Whether to validate the data, only used if apply_filtering and desired_size are not None
     """
     dir_out.mkdir(parents=True, exist_ok=True)
 
     # Read the CSV file and filter
     data = pd.read_csv(csv_path, sep=",")
     if apply_filtering:
-        data = filter_midjourney_data(data, csv_out, desired_size)
+        data = filter_midjourney_data(data, csv_out, desired_size, validate_data)
 
     # Loop through the rows of the DataFrame
     for index, row in tqdm(data.iterrows(), total=data.shape[0], unit="img"):
@@ -81,33 +89,40 @@ def clean_filename(file_name, max_length=100):
     return cleaned_name[:max_length]
 
 
-def test_midjourney_filters(midjourney_csv: Path, csv_out: Optional[Path] = None, desired_size: Optional[int] = None):
+def test_midjourney_filters(midjourney_csv: Path,
+                            csv_out: Optional[Path] = None,
+                            desired_size: Optional[int] = None,
+                            validate_data: bool = True) -> None:
     """
-    Test the output size of the current MidJourney filters, printing the original and final shape.
+    Test the output size of the current Midjourney filters, printing the original and final shape.
 
     Args:
-        midjourney_csv: Path to the MidJourney CSV file
+        midjourney_csv: Path to the Midjourney CSV file
         csv_out: Path for a CSV file to save the filtered data
         desired_size: Desired number of samples (rows) in the output data
+        validate_data: Whether to validate the data during optional size filtering
     """
     og_data = pd.read_csv(midjourney_csv, sep=",")
-    filtered_data = filter_midjourney_data(og_data, csv_out=csv_out, desired_size=desired_size)
+    filtered_data = filter_midjourney_data(og_data, csv_out=csv_out,
+                                           desired_size=desired_size, validate_data=validate_data)
     print(f"Original data: {og_data.shape}")
     print(f"Filtered data: {filtered_data.shape} rows")
 
 
 def filter_midjourney_data(data: pd.DataFrame,
                            csv_out: Optional[Path] = None,
-                           desired_size: Optional[int] = None) -> pd.DataFrame:
+                           desired_size: Optional[int] = None,
+                           validate_data: bool = True) -> pd.DataFrame:
     """
-    Filter the MidJourney data to keep only the rows that pass the filters.
+    Filter the Midjourney data to keep only the rows that pass the filters.
     A CSV file with the filtered data is saved if a path is supplied.
     The output size can be further reduced by specifying a desired size.
 
     Args:
-        data: DataFrame with the MidJourney data
+        data: DataFrame with the Midjourney data
         csv_out: Path for a CSV file to save the filtered data
         desired_size: Desired number of samples in the output data
+        validate_data: Whether to validate the data during optional size filtering
 
     Returns:
         DataFrame with filtered rows and a reduced number of columns
@@ -115,24 +130,37 @@ def filter_midjourney_data(data: pd.DataFrame,
     # Adjustable filters
     # allowed_versions = ["4", "4.0", "5", "5.0", "5.1"]
     allowed_versions = ["5.1"]
-    required_words = ["photo"]  # Prompt having ANY of these subwords is kept
-    blocked_words = ["cartoon", "painting"]  # Prompt having ANY of these subwords is removed
+    required_words = ["photo"]  # Prompt having ANY of these subwords is kept (unless blocked)
+    blocked_words = ["cartoon", "comic", "painting", "drawing", "animation", "sprite"]  # Prompt having ANY of these subwords is removed
     allowed_ratios = ["1:1"]  # Multiples are discarded, such as 2:2 and 3:3
+    max_characters = 1600  # Prompts + parameters with more characters are removed
 
-    data = data[["Unnamed: 0", "Attachments", "version", "aspect", "clean_prompts"]]
+    data = data[["Unnamed: 0", "Attachments", "version", "aspect", "clean_prompts", "Content"]]
     data.rename(columns={"Unnamed: 0": "original_index"}, inplace=True)
     data_length = len(data)
     og_length = data_length
-    print("Filtering MidJourney CSV data...")
+    print("Filtering Midjourney CSV data...")
+    print(f"Original data: {data_length} rows")
+
+    # Character filter
+    # Too long prompts are truncated with "..." with no access to parameters, causing false assumptions
+    data = data[data["Content"].str.len() <= max_characters]
+    print(f"Character filter removed {data_length - len(data)} rows")
+    data_length = len(data)
 
     # Version filter
+    # Default version is 5.1
     data["version"] = data["version"].fillna("5.1").astype(str)
     data = data[data["version"].isin(allowed_versions)]
     print(f"Version filter removed {data_length - len(data)} rows")
     data_length = len(data)
-    # MS Excel shows "5.1" as "5.01.00", similar irrelevant issue with aspect ratio
 
     # Aspect ratio filter
+    # In the original CSV, only --ar has been considered, not --aspect
+    # Replace an empty aspect ratio with a possible found one
+    reg = r"\s--aspect\s(\d+:\d+)(?:\s|\*\*)"
+    data.loc[data["aspect"].isnull(), "aspect"] = data.loc[data["aspect"].isnull(), "Content"].str.extract(reg, expand=False)
+    # Default aspect ratio is 1:1
     data["aspect"] = data["aspect"].fillna("1:1").astype(str)
     data = data[data["aspect"].isin(allowed_ratios)]
     print(f"Aspect ratio filter removed {data_length - len(data)} rows")
@@ -153,16 +181,39 @@ def filter_midjourney_data(data: pd.DataFrame,
     # Desired size filter
     if isinstance(desired_size, int):
         if desired_size < data_length:
-            # Todo: Perform HEAD requests to see that files exist and act accordingly
-            data = data.sample(n=desired_size, random_state=42)
+            if not validate_data:
+                data = data.sample(n=desired_size, random_state=42)
+            else:
+                print("Validating data...")
+                data_new = data.copy()
+                rng = np.random.default_rng(42)
+                running_idx = 0
+                pbar = tqdm(total=desired_size, unit="rows")
+                for idx in rng.choice(data_length, data_length, replace=False):
+                    row = data.iloc[idx]
+                    image_url = row["Attachments"]
+                    response = requests.head(image_url)
+                    if response.status_code == 200:  # Check that the image can be downloaded
+                        data_new.iloc[running_idx] = row
+                        running_idx += 1
+                        pbar.update(1)
+                        if running_idx == desired_size:
+                            break
+                pbar.close()
+                data_new = data_new.iloc[:running_idx]  # Remove unused rows from the copy
+                data = data_new
+                if len(data) < desired_size:
+                    print(f"Warning: not enough valid data {len(data)} to reach desired size, consider changing filters")
+
             print(f"Desired size filter removed {data_length - len(data)} rows")
             data_length = len(data)
+
         elif desired_size == data_length:
             print(f"Desired size filter removed 0 rows")
         elif desired_size > og_length:
             print(f"Desired size is larger than the original data size, no rows removed")
         else:
-            print(f"Desired size is larger than the filtered data size, no rows removed, consider changing filters,")
+            print(f"Desired size is larger than the filtered data size, no rows removed, consider changing filters")
 
     print(f"Final data size: {data_length} rows")
 
@@ -177,9 +228,9 @@ def main():
     midjourney_csv = Path("..", "data", "midjourney_v51_cleaned_data", "upscaled_prompts_df.csv")
     dir_out = Path("..", "data", "midjourney_v51_cleaned_data", "filtered_images")
     csv_out = midjourney_csv.parent.joinpath("filtered_prompts.csv")
-    # test_midjourney_filters(midjourney_csv, csv_out, 1000)
+    # test_midjourney_filters(midjourney_csv, csv_out)
     download_midjourney_data(csv_path=midjourney_csv, dir_out=dir_out,
-                             apply_filtering=True, csv_out=csv_out, desired_size=1000)
+                             apply_filtering=True, csv_out=csv_out, desired_size=1000, validate_data=True)
 
 
 if __name__ == "__main__":
