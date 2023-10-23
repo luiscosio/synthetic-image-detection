7b0444fc14bce5f49954b2791d838586df32d3ba
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-10-23 11:41:36 +0300

    Updated documentation

--- utils/csv_operations.py
+++ utils/csv_operations.py
@@ -6,36 +6,35 @@ import pandas as pd
 
 def create_dataset_csv(images: List, label: int, csv_path: Path, sep: str = ",") -> None:
     """
-    Create a csv file for an image dataset. The csv file contains a row for each image,
+    Create a CSV file for an image dataset. The CSV file contains a row for each image,
     with the image name and the ground truth label. The label is the same for all images.
 
     Args:
         images: List of images
         label: Ground truth label, 0 for real image, 1 for synthesized
-        csv_path: Path to the csv file
-        sep: Separator for the csv file
+        csv_path: Path to the CSV file
+        sep: Separator for the CSV file
     """
     csv_path.parent.mkdir(exist_ok=True, parents=True)
     df = pd.DataFrame(columns=["filename", "label"])
     df["filename"] = [img.name for img in images]
     df["label"] = label
     df.to_csv(csv_path, index=False, sep=sep)
-    return None
 
 
 def add_results_to_csv(csv_path: Path, detector_id: str, results: List, scores: List, sep: str = ",") -> None:
     """
-    Add or overwrite the results of a detector to a dataset csv file.
+    Add or overwrite the results of a detector to a CSV dataset file.
 
     Args:
-        csv_path: Path to the csv file
+        csv_path: Path to the CSV file
         detector_id: ID of the detector
         results: List of labels with the same length as the dataset
         scores: List of scores with the same length as the dataset
-        sep: Separator for the csv file
+        sep: Separator for the CSV file
     """
+    print(f"Saving results to {csv_path.absolute()}...")
     df = pd.read_csv(csv_path, sep=sep)
     df[f"{detector_id}_labels"] = results
     df[f"{detector_id}_scores"] = scores
     df.to_csv(csv_path, index=False, sep=sep)
-    return None
